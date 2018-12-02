572b4158263c92b39abfccaad28a4286716c9c20
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2018-12-03 00:28:43 +0530

    vue animation

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,14 +1,18 @@
 <template>
     <div class="hello">
         <div class="holder">
-            <form @submit.prevent="addSkill" >
-                <input type="text" placeholder="Enter your skills.." v-model="skill">
-                {{skill}}
+            <form @submit.prevent="addSkill">
+                <input type="text" placeholder="Enter your skills.." v-model="skill" v-validate="'min:5'" name="skill">
+                <transition nter-active-class="animated flipInX" leave-active-class="animated flipOutX">
+                    <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+                </transition>
             </form>
             <ul>
-                <li v-for="(data,index) in skills" :key="index">
-                    {{index}}. {{data.skill}}
-                </li>
+                <transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown">
+                    <li v-for="(data,index) in skills" :key="index">
+                        {{index}}. {{data.skill}}
+                    </li>
+                </transition-group>
             </ul>
             <p>These are the skills that you posses</p>
         </div>
@@ -18,20 +22,20 @@
 <script>
     export default {
         name: 'Skills',
-        data(){
-            return{
+        data() {
+            return {
                 skills: [
                     {
-                        "skill" : "Python"
+                        "skill": "Python"
                     },
                     {
-                        "skill" : "Vue.js"
+                        "skill": "Vue.js"
                     },
                     {
-                        "skill" : "Django"
+                        "skill": "Django"
                     },
                     {
-                        "skill" : "Data Analytics"
+                        "skill": "Data Analytics"
                     },
 
                 ],
@@ -41,8 +45,12 @@
         },
         methods: {
             addSkill() {
-                this.skills.push({skill: this.skill});
-                this.skill = '';
+                this.$validator.validateAll().then((result) => {
+                    if (result) {
+                        this.skills.push({skill: this.skill});
+                        this.skill = '';
+                    }
+                });
             }
         }
 
@@ -51,44 +59,44 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+    @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
+    .holder {
+        background: #fff;
+    }
 
-  .holder {
-    background: #fff;
-  }
+    ul {
+        margin: 0;
+        padding: 0;
+        list-style-type: none;
+    }
 
-  ul {
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
-  }
+    ul li {
+        padding: 20px;
+        font-size: 1.3em;
+        background-color: #E0EDF4;
+        border-left: 5px solid #3EB3F6;
+        margin-bottom: 2px;
+        color: #3E5252;
+    }
 
-  ul li {
-    padding: 20px;
-    font-size: 1.3em;
-    background-color: #E0EDF4;
-    border-left: 5px solid #3EB3F6;
-    margin-bottom: 2px;
-    color: #3E5252;
-  }
+    p {
+        text-align: center;
+        padding: 30px 0;
+        color: gray;
+    }
 
-  p {
-    text-align:center;
-    padding: 30px 0;
-    color: gray;
-  }
-
-  .container {
-    box-shadow: 0px 0px 40px lightgray;
-  }
+    .container {
+        box-shadow: 0px 0px 40px lightgray;
+    }
 
     input {
-    width: calc(100% - 40px);
-    border: 0;
-    padding: 20px;
-    font-size: 1.3em;
-    background-color: #323333;
-    color: #687F7F;
-  }
+        width: calc(100% - 40px);
+        border: 0;
+        padding: 20px;
+        font-size: 1.3em;
+        background-color: #323333;
+        color: #687F7F;
+    }
 
 </style>
 
