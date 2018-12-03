f28d9aee5a553088d794a0482274e3552875209f
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2018-12-03 08:47:36 +0530

    vue router - part 2 : use of url parameter

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,13 +1,18 @@
 <template>
     <div class="about">
-        <h1>This is about page</h1>
+        <h1>This is about page and their name is {{ their_name }}</h1>
     </div>
 
 </template>
 
 <script>
     export default {
-        name: "About"
+        name: "About",
+        data() {
+            return {
+                their_name : this.$route.params.name
+            }
+        }
     }
 </script>
 
